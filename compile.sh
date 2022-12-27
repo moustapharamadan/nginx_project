eab3077211ba94b42473e919f0575faa182c91db
moustapharamadan <moustapharamadan@hotmail.com>
2022-12-27 20:50:43 +0200

    full debug symbols

--- compile.sh
+++ compile.sh
@@ -7,20 +7,20 @@ NGINXDIR=$ROOTDIR/nginx #nginx directory
 cd $NGINXDIR
 if [[ "$#" -eq 1 ]]; then
     MODULE_NAME=$1
-    ./configure                       \
-    --with-debug                      \
-    --prefix=$BUILDDIR                \
-    --conf-path=conf/nginx.conf       \
-    --error-log-path=logs/error.log   \
-    --http-log-path=logs/access.log   \
-    --add-module=../$MODULE_NAME     
+    CFLAGS="-g -O0" ./configure                       \
+                    --with-debug                      \
+                    --prefix=$BUILDDIR                \
+                    --conf-path=conf/nginx.conf       \
+                    --error-log-path=logs/error.log   \
+                    --http-log-path=logs/access.log   \
+                    --add-module=../$MODULE_NAME     
 else
-    ./configure                       \
-    --with-debug                      \
-    --prefix=$BUILDDIR                \
-    --conf-path=conf/nginx.conf       \
-    --error-log-path=logs/error.log   \
-    --http-log-path=logs/access.log   
+    CFLAGS="-g -O0" ./configure                       \
+                    --with-debug                      \
+                    --prefix=$BUILDDIR                \
+                    --conf-path=conf/nginx.conf       \
+                    --error-log-path=logs/error.log   \
+                    --http-log-path=logs/access.log   
 fi
  
 make
